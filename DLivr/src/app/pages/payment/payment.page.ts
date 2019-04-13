028adf9704dba9d4ee3d10237dce6208b77d54e1
irimiaandreea <irimia.andreea6@yahoo.com>
2019-04-13 12:48:28 +0300

    payment form

--- DLivr/src/app/pages/payment/payment.page.ts
+++ DLivr/src/app/pages/payment/payment.page.ts
@@ -12,4 +12,27 @@ export class PaymentPage implements OnInit {
   ngOnInit() {
   }
 
+  go(){
+    var x = document.getElementById("formular");
+    var y = document.getElementById("addCard");
+     // x.style.display = "block";
+
+      if (window.getComputedStyle(x).display === 'block') {
+        x.style.display = "none";
+       // y.style.visibility = "hidden";
+        return;
+      }
+      // Otherwise, show it
+      x.style.display = "block";
+      y.style.display = "none";
+  }
+
+  cancel(){
+    var x = document.getElementById("formular");
+    var y = document.getElementById("addCard");
+
+     x.style.display = "none";
+     y.style.display = "block";
+  }
+
 }
