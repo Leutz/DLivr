dac0ec35fff538c8f136e31396ce2f7b9720ff3b
adechan <ade.enter@yahoo.com>
2019-04-14 03:19:02 +0300

    added javascript for mypackages
    partially complete

--- DLivr/src/app/pages/mypackages/mypackages.page.ts
+++ DLivr/src/app/pages/mypackages/mypackages.page.ts
@@ -14,4 +14,56 @@ export class MypackagesPage implements OnInit {
   ngOnInit() {
   }
 
+  closeExistingPackage() {
+    var divAdd = document.getElementById("addPackage");
+    var buttonAdd = document.getElementById("addpackage");
+
+    var divEdit = document.getElementById("editPackage");
+
+    divAdd.style.display = "none";
+    divEdit.style.display = "none";
+
+    //buttonAdd.textContent = "Add Package";
+  }
+
+  addPackageButton(){
+    
+    var divAdd = document.getElementById("addPackage");
+    var buttonAdd = document.getElementById("addpackage");
+    
+    console.log("hello I have the horse teeth");
+    console.log(buttonAdd.textContent);
+
+    this.closeExistingPackage();
+
+    if (buttonAdd.textContent == "Add Package")
+    {
+      divAdd.style.display = "block";
+      buttonAdd.textContent = "Confirm";
+    }
+    else
+    {
+      buttonAdd.textContent = "Add Package";
+    }
+  }
+
+  editPackageForm(){
+    var divEdit = document.getElementById("editPackage");
+    var buttonEdit = document.getElementById("addpackage");
+
+    this.closeExistingPackage();
+
+    divEdit.style.display = "block";
+    buttonEdit.textContent = "Save";
+  }
+
+  cancelEdit(){
+
+    var divEdit = document.getElementById("editPackage");
+    var buttonEdit = document.getElementById("addpackage");
+
+    this.closeExistingPackage();
+
+    buttonEdit.textContent = "Add Package"
+  }
 }
